e29984d25293ef3a26dfd6552c68d82f3766ab75
Tick <dupeng@kofo.io>
2020-03-11 11:06:02 +0800

    init

---
+++ android/app/src/main/java/io/tick/flutter_hotfixdemo/ApplicationContextProvider.java
@@ -0,0 +1,9 @@
+package io.tick.flutter_hotfixdemo;
+
+import android.app.Application;
+import android.content.Context;
+
+public class ApplicationContextProvider {
+    public static Application application = null;
+    public static Context context = null;
+}
---
+++ android/app/src/main/java/io/tick/flutter_hotfixdemo/FlutterPatch.java
@@ -0,0 +1,73 @@
+package io.tick.flutter_hotfixdemo;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.tencent.tinker.lib.tinker.Tinker;
+import com.tencent.tinker.lib.tinker.TinkerLoadResult;
+import com.tencent.tinker.lib.util.TinkerLog;
+import com.tencent.tinker.loader.shareutil.ShareConstants;
+import com.tencent.tinker.loader.shareutil.SharePatchFileUtil;
+
+import java.io.File;
+import java.lang.reflect.Field;
+
+import io.flutter.view.FlutterMain;
+
+public class FlutterPatch {
+
+    private static final String TAG = "FlutterPatch";
+
+    private FlutterPatch() {
+    }
+
+    public static void flutterPatchInit() {
+        try {
+            String libPath = findLibraryFromTinker(ApplicationContextProvider.context,
+                    "lib/armeabi", "libapp.so");
+            Log.e("FlutterPatch", "flutterPatchInit() called   " + libPath);
+            Field field = FlutterMain.class.getDeclaredField("sAotSharedLibraryName");
+            field.setAccessible(true);
+            field.set(null, libPath);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String findLibraryFromTinker(Context context, String relativePath, String libName)
+            throws UnsatisfiedLinkError {
+        final Tinker tinker = Tinker.with(context);
+
+        libName = libName.startsWith("lib") ? libName : "lib" + libName;
+        libName = libName.endsWith(".so") ? libName : libName + ".so";
+        String relativeLibPath = relativePath + "/" + libName;
+        if (tinker.isEnabledForNativeLib() && tinker.isTinkerLoaded()) {
+            TinkerLoadResult loadResult = tinker.getTinkerLoadResultIfPresent();
+            if (loadResult.libs == null) {
+                return libName;
+            }
+            for (String name : loadResult.libs.keySet()) {
+                if (!name.equals(relativeLibPath)) {
+                    continue;
+                }
+                String patchLibraryPath = loadResult.libraryDirectory + "/" + name;
+                File library = new File(patchLibraryPath);
+                if (!library.exists()) {
+                    continue;
+                }
+                //whether we check md5 when load
+                boolean verifyMd5 = tinker.isTinkerLoadVerify();
+                if (verifyMd5 && !SharePatchFileUtil.verifyFileMd5(library,
+                        loadResult.libs.get(name))) {
+                    tinker.getLoadReporter().onLoadFileMd5Mismatch(library,
+                            ShareConstants.TYPE_LIBRARY);
+                } else {
+//                    System.load(patchLibraryPath);
+                    TinkerLog.i(TAG, "findLibraryFromTinker success:" + patchLibraryPath);
+                    return patchLibraryPath;
+                }
+            }
+        }
+        return libName;
+    }
+}
